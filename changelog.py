197105c0bf23cd96192560c77cbad3542ddd1f39
Timi <timi.rautamaki@gmail.com>
2020-11-02 09:24:18 +0100

    changelog: add the script

---
+++ changelog.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+import subprocess
+import os
+
+CHANGELOG_FILENAME = "changelog_{}.txt".format(datetime.today().strftime("%Y%m%d"))
+print("Changelog to file {}".format(CHANGELOG_FILENAME))
+
+# Write changelog
+changelog_file = open(CHANGELOG_FILENAME, "w+")
+log = subprocess.Popen("repo forall -pvc 'git log --after=\"2020-10-27\" --pretty=\"format:%<(12)%h %<(20)%an %s\"'", shell=True, cwd='./', stdout=subprocess.PIPE)
+output = log.stdout.read().decode('utf-8')
+if output != "":
+    print(output)
+    changelog_file.write(output)
